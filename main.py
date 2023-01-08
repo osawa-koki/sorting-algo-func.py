4341945f765404096e776500ff08e68aa0a568fb
osawa-koki <github@osawa.dev>
2023-01-08 20:09:55 +0900

    バブルソート。

---
+++ main.py
@@ -0,0 +1,8 @@
+from app.bubble_sort import bubble_sort
+
+unsorted_list = [5, 3, 8, 6, 7, 2, 1, 4, 9, 0, 5, 3, 8, 6, 7, 2, 1, 4, 9, 0, 2, 5, 7, 1, 0]
+
+print("unsorted list: ", unsorted_list)
+
+print("***** Bubble Sort *****")
+print("sorted list: ", bubble_sort(unsorted_list))
