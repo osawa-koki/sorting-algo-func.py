61853eecd47e86a6940d15335f7706f13707218f
osawa-koki <github@osawa.dev>
2023-01-08 23:58:22 +0900

    ヒープソート。

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app.selection_sort import selection_sort
 from app.insertion_sort import insertion_sort
 from app.merge_sort import merge_sort
 from app.quick_sort import quick_sort
+from app.heap_sort import heap_sort
 
 unsorted_list = [5, 3, 8, 6, 7, 2, 1, 4, 9, 0, 5, 3, 8, 6, 7, 2, 1, 4, 9, 0, 2, 5, 7, 1, 0]
 
@@ -22,3 +23,6 @@ print("sorted list: ", merge_sort(unsorted_list))
 
 print("***** Quick Sort *****")
 print("sorted list: ", quick_sort(unsorted_list))
+
+print("***** Heap Sort *****")
+print("sorted list: ", heap_sort(unsorted_list))
