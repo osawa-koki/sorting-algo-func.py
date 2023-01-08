800d7d7bda8541b7ccf32cb80eb2e55e109d2346
osawa-koki <github@osawa.dev>
2023-01-08 23:51:28 +0900

    クイックソート。

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from app.bubble_sort import bubble_sort
 from app.selection_sort import selection_sort
 from app.insertion_sort import insertion_sort
 from app.merge_sort import merge_sort
+from app.quick_sort import quick_sort
 
 unsorted_list = [5, 3, 8, 6, 7, 2, 1, 4, 9, 0, 5, 3, 8, 6, 7, 2, 1, 4, 9, 0, 2, 5, 7, 1, 0]
 
@@ -18,3 +19,6 @@ print("sorted list: ", insertion_sort(unsorted_list))
 
 print("***** Merge Sort *****")
 print("sorted list: ", merge_sort(unsorted_list))
+
+print("***** Quick Sort *****")
+print("sorted list: ", quick_sort(unsorted_list))
